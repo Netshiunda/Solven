d3ecfaccd0bc839fe033468f25b603dd6d29a09f
Netshiunda <emmanuel.netshiunda@gmail.com>
2021-01-23 21:00:26 +0200

    Testing the one working

--- app.py
+++ app.py
@@ -2,11 +2,38 @@
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
-import os 
+from newspaper import Article
+import random
+import string
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+# Get the article
+article = Article('https://www.solvency.co.za/flexible-insurance-cover.html')
+article.download()
+article.parse()
+article.nlp()
+article_text = article.text
+
+# Print the article text
+#print(article_text)
+text = article_text
+sentence_list = nltk.sent_tokenize(text) # list of sentences
+
 app = Flask(__name__)
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']   #ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN']   #VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
-bot = Bot (ACCESS_TOKEN)
+ACCESS_TOKEN = 'EAADaoWkVeTkBAEevZAojZBvwiUEPNRIytpNbMCPs69ZCfUWlwNbVkvgOgYwEBdobrhe7VQHtyj5ZA3iCslkvqs5LfTzV8rv44UNPMutUiuahJZB8sksiMoFMFCG0ok2O1mMplbFsO03ZAxHqarvR0cIVZBXu7KUuCcZBS2pK41zuz1KOhrbZB4SnD'
+VERIFY_TOKEN = 'TESTINGTOKEN'
+bot = Bot(ACCESS_TOKEN)
+
+# Exit list
+exit_list = ['exit','see you later', 'bye','quit', 'break']
+
+# User's greetings
+user_greetings = ['hi','hey','hello','hola','greetings','wassup']
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
@@ -19,20 +46,26 @@ def receive_message():
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
         # get whatever message a user sent the bot
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+        output = request.get_json()
+        print(output)
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    #Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        if message['message']['text'].lower() in exit_list:
+                            response_sent_text = 'Chat with you later' 
+                        elif message['message']['text'].lower() in user_greetings:
+                            response_sent_text = greeting_response(message['message']['text'])
+                        else:
+                            response_sent_text = bot_response(message['message']['text'])
+                        send_message(recipient_id, response_sent_text)
+                    #if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = bot_response(message['message']['text'])
+                        send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
 
@@ -42,6 +75,74 @@ def verify_fb_token(token_sent):
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
+
+# Create a function to return a random greeting response to a users greeting
+def greeting_response(text):
+    text = text.lower()
+
+    # Bot's greeting response
+    bot_greetings = ['howdy','hi','hey','hello','hola']
+    
+    # User's greetings
+    user_greetings = ['hi','hey','hello','hola','greetings','wassup']
+
+    # Return a randomly choosen response from bot
+
+    for word in text.split():
+        if word in user_greetings:
+            return random.choice(bot_greetings) 
+
+# Index sort function based on similarity in descending order
+def index_sort(list_var):
+    length = len(list_var)
+    list_index = list(range(0,length))
+    
+    x = list_var
+    for i in range(length):
+        for j in range(length):
+            if x[list_index[i]] > x[list_index[j]]:
+                
+                # Swap
+                temp = list_index[i]
+                list_index[i] = list_index[j]
+                list_index[j] = temp
+
+    return list_index
+
+
+# Create the bots response
+
+def bot_response(user_input):
+    user_input = user_input.lower()
+    sentence_list.append(user_input)
+    bot_response = ''
+    # create count matrix
+    cm = CountVectorizer().fit_transform(sentence_list)
+    # compare the last cm entry
+    similarity_scores = cosine_similarity(cm[-1],cm)
+    # Reduce the dimensinality
+    similarity_scores_list = similarity_scores.flatten()
+    index = index_sort(similarity_scores_list)
+    #print(similarity_scores_list)
+    index = index[1:]
+    response_flag = 0
+
+    j = 0
+
+    for i in range(len(index)):
+        if similarity_scores_list[index[i]] > 0.0:
+            bot_response = bot_response + ' ' + sentence_list[index[i]]
+            response_flag = 1
+            j = j + 1
+        if j > 2:
+            break
+    #print(sentence_list)
+    if response_flag == 0 :
+        bot_response = bot_response + ' ' + 'I apologize, I do not understand.'
+    
+    sentence_list.remove(user_input)
+
+    return bot_response
 
 
 #chooses a random message to send to the user
